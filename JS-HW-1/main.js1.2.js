f2919df405f7ef3568c9ebbba3f6aeef7c8ef1e0
Tanya Fedina <korsuntanya606@gmail.com>
2025-02-13 00:14:20 +0200

    Add files via upload

---
+++ JS-HW-1/main.js1.2.js
@@ -0,0 +1,5 @@
+let firstName ='Tatyana';
+let middleName ='Vacheslavovna';
+let lastName ='Fedina';
+let msg = lastName +" " + firstName + " " + middleName;
+console.log(msg)
---
+++ JS-HW-1/main.js1.js
@@ -0,0 +1,2 @@
+let arr = ['hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false]
+console.log(arr)
